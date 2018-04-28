419e0479e053546b8587bd892d1aee1d50c55634
Ahmed <abdulcartel@gmail.com>
2018-04-28 20:47:31 +0300

    linked js properly

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#butt").click(function(event) {
+    var emailInput = $("input#email").val()
+
+
+    event.preventDefault();
+    alert(emailInput + "You have successfully subscribed")
+    $("#butt).html("subscribed!")
+
+  });
+});
